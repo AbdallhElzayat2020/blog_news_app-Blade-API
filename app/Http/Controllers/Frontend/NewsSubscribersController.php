485d0f2d04bb73bec7a06da12017a048320a5b25
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-05-30 12:50:18 +0300

    Refactors newsletter subscription process to use a dedicated email view and implements database transaction handlingAdds newsletter subscription confirmation email and transaction handling
    Introduces a new email template for newsletter subscription confirmations. Updates the `NewsSubscribersController` to include transactional database handling and email dispatching for better data consistency. Modifies `NewsSubscriberMail` to use the new email view.

--- app/Http/Controllers/Frontend/NewsSubscribersController.php
+++ app/Http/Controllers/Frontend/NewsSubscribersController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Mail\NewsSubscriberMail;
 use App\Models\NewsSubscriber;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 class NewsSubscribersController extends Controller
@@ -18,6 +19,8 @@ class NewsSubscribersController extends Controller
                 'email' => ['required', 'email', 'unique:news_subscribers,email']
             ]);
 
+            DB::beginTransaction();
+
             $email = NewsSubscriber::create([
                 'email' => $request->email,
             ]);
@@ -25,13 +28,16 @@ class NewsSubscribersController extends Controller
             if (!$email) {
                 return redirect()->back()->with('error', 'Something went wrong, please try again later.');
             }
+
+            Mail::to($request->email)->send(new NewsSubscriberMail());
+            DB::commit();
         } catch (\Illuminate\Validation\ValidationException $e) {
+            DB::rollBack();
             return redirect()->back()->with('error', $e->getMessage());
         }
 
-//        Mail::to($request->email)->send(new NewsSubscriberMail());
+        //        Mail::to($request->email)->send(new NewsSubscriberMail());
 
         return redirect()->back()->with('success', 'You have successfully subscribed to our newsletter.');
-
     }
 }
