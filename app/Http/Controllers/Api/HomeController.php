6d341825503e35c94cfdff844d78faab32dd86cf
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-07-09 07:44:14 +0300

    * Get posts for the API      *      * Retrieves various types of posts including      * - All active posts      * - Latest posts (4)      * - Categories with their latest posts (4 per category)      * - Most read posts (3)      * - Oldest posts (3)      * - Popular posts based on comment count (3)

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -3,15 +3,48 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    /**
+     * Get posts for the API
+     * 
+     * Retrieves various types of posts including:
+     * - All active posts
+     * - Latest posts (4)
+     * - Categories with their latest posts (4 per category)
+     * - Most read posts (3)
+     * - Oldest posts (3)
+     * - Popular posts based on comment count (3)
+     * 
+     * @return \Illuminate\Http\JsonResponse Returns popular posts as JSON
+     */
     public function getPosts()
     {
-        $posts = Post::active()->get();
-        return response()->json($posts);
+        $query = Post::query();
+
+        $posts = $query->active()->latest()->get();
+
+        $latest_posts = $query->latest()->take(4)->get();
+
+        $categories = Category::all();
+        $category_with_posts = $categories->map(function (Category $category) {
+            $category->posts = $category->posts()->active()->latest()->take(4)->get();
+            return $category;
+        });
+
+        $most_read_posts = $query->orderBy('number_of_views', 'desc')->take(3)->get();
+
+        $oldest_posts = $query->oldest()->take(3)->get();
+
+        $popular_posts = $query->withCount(['comments'])
+            ->orderBy('comments_count', 'desc')
+            ->take(3)
+            ->get();
+
+        return response()->json($popular_posts);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\HomeController;
-use App\Models\Post;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
